093aa3b4c804c9a116bcb7179023bbae247a378d
Thomas Steckmann <67061034+tmsteck@users.noreply.github.com>
2026-04-10 16:27:23 -0400

    Change base path for deployment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Use relative asset paths so deploys work at root or subpath.
-  base: './',
+  base: '/Project-agent/',
 })
